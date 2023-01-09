61663142d81eb84fe715035fe8233e1f86dba270
Akhilesh Yethirajam <33382779+yethirak@users.noreply.github.com>
2023-01-09 11:53:45 +0800

    Completed exercise 2

--- src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
@@ -1,5 +1,7 @@
 package io.javabrains.reactiveworkshop;
 
+import reactor.core.publisher.Flux;
+
 import java.io.IOException;
 
 public class Exercise2 {
@@ -9,10 +11,17 @@ public class Exercise2 {
         // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
 
         // Print all numbers in the ReactiveSources.intNumbersFlux stream
-        // TODO: Write code here
+        System.out.println("Print all numbers in the ReactiveSources.intNumbersFlux stream");
+        ReactiveSources.intNumbersFlux().subscribe(System.out::println);
+        //underneath it behaes like below
+        Flux<Integer> stream = ReactiveSources.intNumbersFlux();
+        stream.subscribe(e -> System.out.println(e));
+        //multiple subscribes
+        stream.subscribe(e -> System.out.println("Another Stream" + e));
 
         // Print all users in the ReactiveSources.userFlux stream
-        // TODO: Write code here
+        System.out.println("Print all users in the ReactiveSources.userFlux stream");
+        ReactiveSources.userFlux().subscribe(System.out::println);
 
         System.out.println("Press a key to end");
         System.in.read();
